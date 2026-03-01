ff39cbdd9f362d3590944a7e20af8d33125b5071
Israel Corrales Borrego <icorbor@g.educaand.es>
2026-03-01 22:48:17 +0100

    Fix: Orden de CORS para imagenes

--- BACK/src/app.js
+++ BACK/src/app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const mortifagosRoutes = require("./routes/mortifagos.routes");
+
 const app = express();
-app.use("/img", express.static(path.join(__dirname, "../public/img")));
-app.use(cors());
+
+app.use(cors()); 
+
 app.use(express.json());
+
+app.use("/img", express.static(path.join(__dirname, "../public/img")));
+
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
 });
-app.use("/api/mortifagos", mortifagosRoutes);
-module.exports = app;
 
+app.use("/api/mortifagos", mortifagosRoutes);
+
+module.exports = app;
